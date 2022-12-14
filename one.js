ba8eed32cb07e8652a616e6c667307a65159d932
akhilagarlapati <akhilagarlapati.2121@gmail.com>
2022-12-14 13:25:31 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,11 @@
+const age=15;
+if(age>=18){
+    console.log('Akhila can get driving license')
+}else{
+    const yearsleft=18-age;
+    console.log(`still ${yearsleft} years left to drive`)
+}
+let age2=18;
+console.log(age2)
+let ename='akhi'
+console.log(ename)
