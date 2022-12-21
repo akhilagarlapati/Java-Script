847894d08286387853547c20fabb33545e66eaa0
akhilagarlapati <akhilagarlapati.2121@gmail.com>
2022-12-21 17:03:47 +0530

    ok

---
+++ iterative st-Assignments.js
@@ -0,0 +1,31 @@
+
+//program to print 1 to 10 numbers using a while loop
+let i=1
+while(i<=10){
+    console.log(i)
+    i++
+}
+
+//program to print 10 to 1 numbers using a while loop
+let j=10
+ while(j>0){
+    console.log(j)
+   j--
+}
+
+//program to print 15 to 10 numbers using for loop
+for(i=15;i>=10;i--){
+    console.log(i);
+}
+
+//program to print 1 to 10 odd numbers using for loop
+for(i=1;i<=10;i=i+2){
+    console.log(i);
+}
+
+//program to print first ten multiples of 4 using a while loop
+let k=4;
+while(k<=16){
+    console.log('4*',k,'=',4*k)    
+    k=k+4
+}
