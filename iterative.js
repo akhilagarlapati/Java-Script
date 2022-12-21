e62c3f97385df11dabc4c03c0eee98e4d92de3e2
akhilagarlapati <akhilagarlapati.2121@gmail.com>
2022-12-21 18:24:29 +0530

    pending

--- iterative.js
+++ iterative.js
@@ -29,3 +29,35 @@ while(k<=16){
     console.log('4*',k,'=',4*k)    
     k=k+4
 }
+console.log('***********')
+//Write a program to print 1 to 10 odd numbers using while Loop
+let h=1;
+while(h<10){
+    console.log(h)
+    h=h+2
+}
+
+// Write a program to print 1 to 10 numbers using a do while loop
+let g=1
+do{
+    console.log(g)
+    g++
+}
+while(g<=10)
+
+console.log('***********')
+//Write a program to print 1 to 10 even numbers using a do while loop
+let H=2;
+do{
+    console.log(H)
+    H=H+2
+}while(H<=10)
+console.log('***********')
+//Write a program to print a multiplication table of 6 using a while loop.
+let G=1;
+let K=6
+do{
+    console.log('6*',G,'=',K)
+    K=K+6
+    G=G+1
+}while(K<65)
