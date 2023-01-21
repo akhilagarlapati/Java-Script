48669003e6b8446bcc3b174c5bbcd9d6eb813170
akhilagarlapati <akhilagarlapati.2121@gmail.com>
2023-01-21 13:30:59 +0530

    pending

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,2 +1,24 @@
 'use strict';
+let btnsOpenModal=document.querySelectorAll('.show-modal')
+let btnCloseModal=document.querySelector('.close-modal')
+let overlay=document.querySelector('.overlay')
+let Modal=document.querySelector('.modal')
+console.log(btnsOpenModal);
+
+const openModal=function(){
+    Modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+for(let i=0;i<btnsOpenModal.length;i++){
+    (btnsOpenModal[i].addEventListener('click',openModal))
+}
+
+const closeModal=function(){
+    Modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+btnCloseModal.addEventListener('click',closeModal)
+overlay.addEventListener('click',closeModal)
 
